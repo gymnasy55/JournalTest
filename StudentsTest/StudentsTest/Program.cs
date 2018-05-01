47d223200f629be7b52f65f5a63c9a162da8d30e
gymnasy55 <school55code@gmail.com>
2018-05-01 21:59:57 +0300

    EDIT 0.1

--- StudentsTest/StudentsTest/Program.cs
+++ StudentsTest/StudentsTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using StudentBL;
@@ -11,15 +12,27 @@ namespace StudentsTest
         static void Main(string[] args)
         {
             string fileName = "students.txt";
+            string[] subjects = {"Math", "Ukrainian language", "Ukrainian literature", "PE"};
             List<Student> students = Student.ReadStudents(fileName);
             while (true)
             {
-                Console.Write("Выберите, что хотите сделать (1 - добавить, 0 - удалить список, -1 - выход из программы): ");
+                Console.Write("Выберите, что хотите сделать (2 - выбрать ученика, 1 - добавить, 0 - удалить список, -1 - выход из программы): ");
                 int mode = -1;
                 mode = Convert.ToInt32(Console.ReadLine());
                 if (mode == 0)
                 {
                     for (int i = students.Count - 1; i >= 0; i--) students.RemoveAt(i);
+                    DirectoryInfo mainDir = new DirectoryInfo("students/9/");
+                    DirectoryInfo[] dirs = mainDir.GetDirectories();
+                    foreach (var dir in dirs)
+                    {
+                        FileInfo[] files = dir.GetFiles();
+                        foreach (var file in files)
+                        {
+                            file.Delete();
+                        }
+                        dir.Delete();
+                    }
                 }
                 else if (mode == 1)
                 {
@@ -29,6 +42,35 @@ namespace StudentsTest
                     students.Add(tempStudent);
                 }
                 else if (mode == -1) break;
+                else if (mode == 2)
+                {
+                    for (int i = 0; i < students.Count; i++)
+                    {
+                        Console.WriteLine((i + 1).ToString() + "-" + students[i].Name + " " + students[i].Surname + " " + students[i].Patronomyc + " " + students[i].Age.ToString() + " " + students[i].Grade.ToString());
+                    }
+                    Console.Write("Выберите номер студента: ");
+                    int NumberOfStudent = Convert.ToInt32(Console.ReadLine());
+                    NumberOfStudent--;
+                    Console.Write("Выберите режим работы с учеником (1 - работа с оценками, 0 - удалить студента): ");
+                    int markMode = Convert.ToInt32(Console.ReadLine());
+                    if (markMode == 0)
+                    {
+                        students.RemoveAt(NumberOfStudent);
+                    }
+                    else if (markMode == 1)
+                    {
+                        Console.Write("Выберите предмет из списка: ");
+                        foreach (var _subject in subjects)
+                        {
+                            Console.Write(_subject+" ");
+                        }
+
+                        string subject = Console.ReadLine();
+                        Console.Write("Выберите режим работы с оценками (1 - добавить, 0 - удалить): ");
+                        
+
+                    }
+                }
             }
             Student.WriteStudents(students, fileName);
         }
